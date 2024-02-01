1622200e5960a2531c55bef32a9a7f1e1ca9c5d8
celinemacairan <cmacaira@terpmail.umd.edu>
2024-01-31 19:26:59 -0500

    Added code to demonstrate a first code change.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Celine Macairan!'
+    return 'Hello World from Celine Macairan! This is my first code change.'
 
 
 if __name__ == '__main__':
