1470a99e467a578a4bd1a12787c84a7b372f3d09
celinemacairan <cmacaira@terpmail.umd.edu>
2024-03-28 17:40:00 -0400

    Added /favorite-course route.

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import request
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World from Celine Macairan! This is my first code change.'
+
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/about-css')
+def about_css():
+    return render_template('about-css.html')
+
+
+@app.route('/favorite-course', methods=['GET'])
+def favorite_course():
+    subject = request.args.get('subject', 'BMGT')
+    course_number = request.args.get('course_number', '407')
+    print('subject entered', subject)
+    print('course_number entered', course_number)
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run()
